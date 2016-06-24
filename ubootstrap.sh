a62cc878b17697b9a288b138ac34e0019e515b2d
Piotr Król <piotr.krol@3mdeb.com>
2016-06-24 14:43:19 +0200

    ubootstrap.sh: add vimrc.local

--- ubootstrap.sh
+++ ubootstrap.sh
@@ -3,4 +3,5 @@ ln -s ${PWD}/gitconfig ${HOME}/.gitconfig
 git clone https://github.com/pietrushnic/spf13-vim.git ${HOME}/src/spf13-vim
 ln -s ${HOME}/src/spf13-vim/.vimrc.before.local ${HOME}/.vimrc.before.local
 ln -s ${HOME}/src/spf13-vim/.vimrc.bundles.local ${HOME}/.vimrc.bundles.local
+ln -s ${HOME}/src/spf13-vim/.vimrc.local ${HOME}/.vimrc.local
 ${HOME}/src/spf13-vim/bootstrap.sh
