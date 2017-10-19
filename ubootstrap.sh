ca7ab506fa18ee7edca6a508ccf67b9fed23e4dd
Piotr Król <piotr.krol@3mdeb.com>
2017-10-19 03:04:38 +0200

    ubootstrap.sh: fix symbolic links and git uris
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- ubootstrap.sh
+++ ubootstrap.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 ln -s ${PWD}/gitconfig ${HOME}/.gitconfig
 ln -s ${PWD}/tmux.conf ${HOME}/.tmux.conf
-git clone https://github.com/pietrushnic/spf13-vim.git ${HOME}/src/spf13-vim
+git clone git@github.com:pietrushnic/spf13-vim.git ${HOME}/src/spf13-vim
 ln -s ${HOME}/src/spf13-vim/.vimrc.before.local ${HOME}/.vimrc.before.local
 ln -s ${HOME}/src/spf13-vim/.vimrc.bundles.local ${HOME}/.vimrc.bundles.local
 ln -s ${HOME}/src/spf13-vim/.vimrc.local ${HOME}/.vimrc.local
-${HOME}/src/spf13-vim/bootstrap.sh
+ln -s ${HOME}/src/spf13-vim ${HOME}/.spf13-vim-3
+${HOME}/.spf13-vim-3/bootstrap.sh
+git clone git@github.com:pietrushnic/oh-my-zsh.git
+ln -s /home/${USER}/src/oh-my-zsh ~/.oh-my-zsh
+ln -s /home/${USER}/src/oh-my-zsh/zshrc ~/.zshrc
